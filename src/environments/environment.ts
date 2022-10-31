6686fba76c88e75eabe3eca4a0ed49e5832e9ef9
AkritiAwasthi <akriti.awasthi96@gmail.com>
2022-10-31 14:55:03 +0530

    Environment.ts added

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  weatherAPIBaseUrl: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
+  XRapidAPIHostHeaderName: "X-RapidAPI-Host",
+  XRapidAPIHostHeaderValue: "weather-by-api-ninjas.p.rapidapi.com",
+  XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+  XRapidAPIKeyHeaderValue: "f547b2459bmshce50e59ac3836b1p1721b1jsn322c24770ab5"
+
+};
